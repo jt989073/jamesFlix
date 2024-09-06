fa5ea51a104a2f8be49cec2aa7839ce6cbad5c46
jt989073 <thompsonjames0120@gmail.com>
2024-09-06 12:04:58 -0500

    finished logout and signup route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,9 +1,9 @@
 import { User } from "../models/user.model.js";
-import bcryptjs from 'bcryptjs'
+import bcryptjs from "bcryptjs";
+import { generateTokenAndSetCookie } from "../utils/generateToken.js";
 
 export const signup = async (req, res, next) => {
   try {
-
     const { username, email, password } = req.body;
 
     if (!username || !email || !password)
@@ -19,44 +19,71 @@ export const signup = async (req, res, next) => {
 
     // must have length of 8 characters, have an uppercase and lowercase letter,\ and a special character
     const passwordRegex =
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*_?&])[A-Za-z\d@$!%*_?&]{8,}$/;
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*_?&])[A-Za-z\d@$!%*_?&]{8,}$/;
 
     if (!passwordRegex.test(password)) {
-        console.log('first')
-      return res.status(400).json({ success: false, message: "must have length of 8 characters, have an uppercase and lowercase letter, and a special character" });
+      console.log("first");
+      return res
+        .status(400)
+        .json({
+          success: false,
+          message:
+            "must have length of 8 characters, have an uppercase and lowercase letter, and a special character",
+        });
     }
 
-    const existingUserEmail = await User.findOne({email: email})
+    const existingUserEmail = await User.findOne({ email: email });
 
-    if(existingUserEmail){
-        console.log('2')
-        return res.status(400).json({ success: false, message: "Email already exists" });
+    if (existingUserEmail) {
+      console.log("2");
+      return res
+        .status(400)
+        .json({ success: false, message: "Email already exists" });
     }
 
-    const existingUserUsername = await User.findOne({username: username})
+    const existingUserUsername = await User.findOne({ username: username });
 
-    if(existingUserUsername){
-        console.log(3)
-        return res.status(400).json({ success: false, message: "username already exists." });
+    if (existingUserUsername) {
+      console.log(3);
+      return res
+        .status(400)
+        .json({ success: false, message: "username already exists." });
     }
 
-    const profilePics = ["/avatar1.png", "/avatar2.png", "/avatar3.png"]
-    const image = profilePics[Math.floor(Math.random() * profilePics.length)]
+    const profilePics = ["/avatar1.png", "/avatar2.png", "/avatar3.png"];
+    const image = profilePics[Math.floor(Math.random() * profilePics.length)];
 
-    const salt = await bcryptjs.genSalt(10)
-    const hashedPassword = await bcryptjs.hash(password, salt)
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
 
-    const newUser = new User({email, password: hashedPassword, username, image})
-    await newUser.save()
+    const newUser = new User({
+      email,
+      password: hashedPassword,
+      username,
+      image,
+    });
 
-    return res.status(201).json({success: true, user: {...newUser._doc, password: ''}})
-
+    generateTokenAndSetCookie(newUser._id, res);
+    await newUser.save();
+    return res
+      .status(201)
+      .json({ success: true, user: { ...newUser._doc, password: "" } });
   } catch (error) {
-    console.log('Error in signup controller: ',error.message)
-    return res.status(500).json({success: false, message: 'Internal Server eerror'})
+    console.log("Error in signup controller: ", error.message);
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal Server eerror" });
   }
 };
 
 export const login = async (req, res, next) => {};
 
-export const logout = async (req, res, next) => {};
+export const logout = async (req, res, next) => {
+  try {
+    res.clearCookie('jwt-jamesFlix')
+   return  res.status(200).json({success: true, message: 'Logout out successfully'})
+  } catch (error) {
+    console.log("Error in logout controlller: ", error.message)
+    return res.status(500).json({success: false, message: 'Internal Server Error'})
+  }
+};

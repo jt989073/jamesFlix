61dc869469739f2d5253f7d30fbc3a7c23cd3c11
jt989073 <thompsonjames0120@gmail.com>
2024-09-12 10:34:50 -0500

    final update to serve the correct path to the frontend

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,11 @@ app.use("/api/tv",protectRoute, tvRoutes);
 app.use("/api/search",protectRoute, searchRoutes);
 
 if(ENV_VARS.NODE_ENV === 'production'){
-  
+  app.use(express.static(path.join(__dirname, '/frontend/dist')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+  })
 }
 
 app.use((req, res) => {
